48fd80ce9b9d42bc16ebc24907270cf4e02f6fcf
federico-init <fefe.borrometi@gmail.com>
2023-10-15 16:05:48 +0200

    chore: styled components creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+  border: 1px solid red;
+`;
+const Wrapper = styled.div``;
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>video cards</Wrapper>
+      </Main>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  flex: 1;
+  background-color: #202020;
+`;
+
+const Menu = () => {
+  return <Container>Menu</Container>;
+};
+
+export default Menu;
